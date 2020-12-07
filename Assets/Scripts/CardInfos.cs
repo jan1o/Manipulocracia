f7ffb9381a8d947d8cadec64c056f909d311db10
Janio Fernandes <janiojr14@hotmail.com>
2020-12-07 09:49:38 -0300

    criacao do sistema de manipulacao de atributos do player

---
+++ Assets/Scripts/CardInfos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfos : MonoBehaviour
+{
+    public string nome;
+    public int favoraveis;
+    public int desfavoraveis;
+    public int popularidade;
+    public int hate;
+    public int dinheiro;
+
+    public Sprite imagem;
+
+    public List<int> getAtributes()
+    {
+        List<int> atributes = new List<int>();
+        atributes.Add(favoraveis); atributes.Add(desfavoraveis); atributes.Add(popularidade); atributes.Add(hate); atributes.Add(dinheiro);
+        return atributes;
+    }
+}
---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    #region Singleton
+    public static PlayerStatus instance;
+
+    void Awake()
+    {
+
+        instance = this;
+    }
+
+    public void AnularInstance()
+    {
+        instance = null;
+    }
+    #endregion
+
+    #region atributos correntes do jogador
+    public int favoraveis;
+    public int desfavoraveis;
+    public int popularidade;
+    public int hate;
+    public int dinheiro;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.favoraveis = 5;
+        this.desfavoraveis = 5;
+        this.popularidade = 5;
+        this.hate = 5;
+        this.dinheiro = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void AlterarAtributos(List<int> alteradores)
+    {
+
+    }
+
+
+}
